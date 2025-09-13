7f787ce82460f85b0cec77c8b9ae26f135c82bba
Qasim Khayal <qasimkhiyal4@gmail.com>
2025-09-13 20:59:59 +0530

    JS basics Tables and Pattern

--- tables.js
+++ tables.js
@@ -1,5 +1,9 @@
-let n = 2;
-for (let i = 1; i <= 10; i++){
-    let rsult = n * i;
+for (let n = 1; n <= 10; n++) {
+  console.log("printing Multiplication table of " + n);
+  console.log();
+  for (let i = 1; i <= 10; i++) {
+    let result = n * i;
     console.log(n + " x " + i + " = " + result);
+  }
+  console.log();
 }
