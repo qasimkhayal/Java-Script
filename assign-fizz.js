3089c571ea0ad0acf947ae47a116ce78b45e8967
Qasim Khayal <qasimkhiyal4@gmail.com>
2025-09-20 13:01:45 +0530

    Assignment 04

---
+++ assign-fizz.js
@@ -0,0 +1,11 @@
+for (let n = 1; n <= 50; n++) {
+  if (n % 3 == 0 && n % 5 == 0) {
+    console.log(n + "FIZZBUZZ");
+  } else if (n % 3 == 0) {
+    console.log(n + "FIZZ");
+  } 
+//   else {(n % 5 == 0)
+//     console.log( n + "BUZZ")
+//   }
+  
+}
---
+++ odd-evan-assign.js
@@ -0,0 +1,7 @@
+for (let n = 1; n <= 20; n++) {
+  if (n % 2 == 0) {
+    console.log(n + "even number");
+  } else {
+    console.log(n + "odd number");
+  }
+}
